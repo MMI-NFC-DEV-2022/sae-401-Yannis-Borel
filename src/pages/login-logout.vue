f609636de76ab03f3a48f4f96ab3c4549aad65c7
Yannis-Borel <yannis.borel@edu.univ-fcomte.fr>
2024-03-25 12:24:55 +0100

    push

--- src/pages/login-logout.vue
+++ src/pages/login-logout.vue
@@ -2,12 +2,12 @@
 import { supabase, user } from '../supabase';
 </script>
 <template>
-<div>
-    <button class="mt-28" v-if="user" @pointerdown="supabase.auth.signOut()">
-        Se déconnecter ({{user.email}})
-    </button>
-    <button v-else @pointerdown="supabase.auth.signInWithOAuth({provider: 'github'})">
-        Se connecter avec Github
-    </button>
-</div>
+    <div class="flex align-middle justify-center">
+        <button class="mt-28" v-if="user" @pointerdown="supabase.auth.signOut()">
+            Se déconnecter ({{user.email}})
+        </button>
+        <button class="mt-28" v-else @pointerdown="supabase.auth.signInWithOAuth({provider: 'github'})">
+            Se connecter avec Github
+        </button>
+    </div>
 </template>
